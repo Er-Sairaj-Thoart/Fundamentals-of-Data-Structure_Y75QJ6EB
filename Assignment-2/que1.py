703d2bcf617de4d02d389d9286169830d46e2077
Er-Sairaj-Thoart <sairajthorat1221@gmail.com>
2026-01-22 13:55:27 +0530

    Update que1.py

--- Assignment-2/que1.py
+++ Assignment-2/que1.py
@@ -1,9 +1,4 @@
-# que1.py
+def SumFunction(number1, number2):
+	return number1+number2
 
-"""
-Define a function which can compute the sum of two numbers.
-
-Hints: Define a function with two numbers as arguments. You can compute the sum in the function and return the value.
-"""
-
-# Start coding below...
+print(SumFunction(1,2))
