09a189cfa70909c7a8eba1e0f99b6d887a9d637f
Er-Sairaj-Thoart <sairajthorat1221@gmail.com>
2026-01-22 13:53:20 +0530

    Init: Assignment 1

---
+++ Assignment-1/q3.py
@@ -0,0 +1,9 @@
+# q3.py
+
+"""
+Question: Write a method which can calculate square value of number
+
+Hints: Using the ** operator
+"""
+
+# Start coding below...
---
+++ Assignment-1/q5.py
@@ -0,0 +1,8 @@
+# q5.py
+
+"""
+Question: Define a class, which have a class parameter and have a same instance parameter.
+Hints: Define a instance parameter, need add it in init method You can init a object with construct parameter or set the value later
+"""
+
+# Start coding below...
